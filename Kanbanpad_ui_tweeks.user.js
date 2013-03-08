5bb7606d9fc182c621378ee7490172c8fe4ef47d
David Grayston <davidgrayston@dennis.co.uk>
2013-03-08 11:04:26 +0000

    Style fixes

--- Kanbanpad_ui_tweeks.user.js
+++ Kanbanpad_ui_tweeks.user.js
@@ -21,6 +21,23 @@ $(document).ready(function() {
     $("#user-swatch").prepend($('#' + avatar_id));
   }
   
+  // style fixes
+  var head = document.getElementsByTagName('head')[0];
+  var s = document.createElement('style');
+  var css = 'body.project .kanban-column ul li .title{font-size:10px; line-height:14px; width:80%;}';
+  css += 'body.project .kanban-column ul li .user img{height:80px; width:80px;}';
+  css += 'body.project .kanban-column ul li .unassigned{display:none;}';
+  css += 'body.project .kanban-column ul li{min-height:15px;}';
+  css += 'body.project .kanban-column ul li p.actions{right:0; top:0; position:absolute; width:55px;}';
+  css += 'body.project .kanban-column span.asignee{margin-left:12px; float:left;}';
+  s.setAttribute('type', 'text/css');
+  if (s.styleSheet) {   // IE
+      s.styleSheet.cssText = css;
+  } else {                // the world
+      s.appendChild(document.createTextNode(css));
+  }
+  head.appendChild(s);
+  
   // Hide the feedback button.
   //$('#feedback-button').hide();
 });
